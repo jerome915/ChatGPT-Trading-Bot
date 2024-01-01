86ffaa5b8af704356ae5df5154633ab1ee00e210
jerome <jie0915@gmail.com>
2024-01-02 00:19:04 +0800

    test: 測試網頁

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
-from flask import Flask, request
-import requests
-import json
-import os
+# from flask import Flask, request
+# import requests
+# import json
+# import os
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-if __name__ == "__main__":
-	app.run(port="7777",debug=True)
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+# if __name__ == "__main__":
+# 	app.run(port="7777",debug=True)
 
-@app.route('/', methods=['GET'])
-def home():
-    return 'Hello, World!'
+# @app.route('/', methods=['GET'])
+# def home():
+#     return 'Hello, World!'
 
 
-if __name__ == "__main__":
-    print(f"My IP: {requests.get('https://api.my-ip.io/ip').text}")
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT')))
+# if __name__ == "__main__":
+#     print(f"My IP: {requests.get('https://api.my-ip.io/ip').text}")
+#     app.run(host='0.0.0.0', port=int(os.environ.get('PORT')))
