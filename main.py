8ccf8618211c81fe8b1e85cb1904bb12b31bfdf0
jerome <jie0915@gmail.com>
2024-01-02 00:37:31 +0800

    test: 移除註解

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 from flask import Flask, request
 import requests
 
-# import json
+import json
 import os
-# from src.trading import BinanceTrading
-# from src.logger import logger
+from src.trading import BinanceTrading
+from src.logger import logger
 
 app = Flask(__name__)
 
-# binance_trading = BinanceTrading(os.environ.get('API_KEY'), os.environ.get('API_SECRET_KEY'))
+binance_trading = BinanceTrading(os.environ.get('API_KEY'), os.environ.get('API_SECRET_KEY'))
 
 
-# @app.route('/webhook', methods=['POST'])
-# def tradingview_request():
-#     data = json.loads(request.data)
-#     logger.info(data)
-#     if data.get('passphrase', None) != os.environ.get('PASSPHRASE'):
-#         logger.warning('Wrong passphrase')
-#         return {
-#             'code': 'error',
-#             'message': 'Invalid passphrase'
-#         }
+@app.route('/webhook', methods=['POST'])
+def tradingview_request():
+    data = json.loads(request.data)
+    logger.info(data)
+    if data.get('passphrase', None) != os.environ.get('PASSPHRASE'):
+        logger.warning('Wrong passphrase')
+        return {
+            'code': 'error',
+            'message': 'Invalid passphrase'
+        }
 
-#     symbol = data.get('symbol')
-#     leverage = data.get('leverage')
-#     quantity = data.get('quantity')
-#     max_quantity_ratio = data.get('max_quantity_ratio')
-#     logger.info(f'symbol:{symbol} ,leverage: {leverage}, quantity: {quantity}, max_quantity_ratio: {max_quantity_ratio}, message: {data.get("message")}')
-#     if data.get('message') == 'Sell' or data.get('message') == 'Buy_Exit':
-#         logger.info(f'SELL: {symbol}, leverage: {leverage}, quantity: {quantity}, max_quantity_ratio: {max_quantity_ratio}')
-#         binance_trading.sell(symbol, leverage, quantity, max_quantity_ratio)
-#     elif data.get('message') == 'Buy' or data.get('message') == 'Sell_Exit':
-#         logger.info(f'BUY: {symbol}, leverage: {leverage}, quantity: {quantity}, max_quantity_ratio: {max_quantity_ratio}')
-#         binance_trading.buy(symbol, leverage, quantity, max_quantity_ratio)
-#     return {"message": "successful"}
+    symbol = data.get('symbol')
+    leverage = data.get('leverage')
+    quantity = data.get('quantity')
+    max_quantity_ratio = data.get('max_quantity_ratio')
+    logger.info(f'symbol:{symbol} ,leverage: {leverage}, quantity: {quantity}, max_quantity_ratio: {max_quantity_ratio}, message: {data.get("message")}')
+    if data.get('message') == 'Sell' or data.get('message') == 'Buy_Exit':
+        logger.info(f'SELL: {symbol}, leverage: {leverage}, quantity: {quantity}, max_quantity_ratio: {max_quantity_ratio}')
+        binance_trading.sell(symbol, leverage, quantity, max_quantity_ratio)
+    elif data.get('message') == 'Buy' or data.get('message') == 'Sell_Exit':
+        logger.info(f'BUY: {symbol}, leverage: {leverage}, quantity: {quantity}, max_quantity_ratio: {max_quantity_ratio}')
+        binance_trading.buy(symbol, leverage, quantity, max_quantity_ratio)
+    return {"message": "successful"}
 
 
 @app.route('/', methods=['GET'])
