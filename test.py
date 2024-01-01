0e36a0fa1f570ba0adc7e6d2ca3116b8e9cbdb73
jerome <jie0915@gmail.com>
2024-01-01 23:57:33 +0800

    test

---
+++ test.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import requests
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+if __name__ == "__main__":
+	app.run(host="0.0.0.0",port="5000",debug=True)
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'Hello, World!'
+
+
+if __name__ == "__main__":
+    print(f"My IP: {requests.get('https://api.my-ip.io/ip').text}")
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT')))
